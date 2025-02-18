bdfb641299ee1a06d0cff06aa9d8d957e635f7eb
Alexander Khudyakov <89903767+Miner113@users.noreply.github.com>
2025-02-18 19:22:33 +0300

    Add files via upload

---
+++ src/revs.cpp
@@ -0,0 +1,22 @@
+#include "revs.hpp" 
+
+const std::string NAME = "MNSE";
+const std::string SUPPLY = "100000";
+const std::string REWARD = "100000000,25000000000,210000000";
+const std::string END = "100,1000,10000000";
+const std::string ADAPTIVEPOW = "6";
+const std::string ERAS = "3";
+const std::string BLOCKTIME = "30";
+const std::string Cbmaturity = "3";
+const std::string STAKED = "10";
+const std::string SAPLING = "1";
+const std::string PERC = "40000000";
+const std::string PUBKEY = "02d7296c4c34c1e3dbf383cf8534da4359cc6ef87e249e0c64f97b02b5405472ff";
+const std::string ADDNODES[] = {
+    "node.moonsnake.org",
+    "node1.moonsnake.org",
+    "node2.moonsnake.org",
+    "node3.moonsnake.org",
+    "node4.moonsnake.org"
+};
+const int ADDNODES_COUNT = sizeof(ADDNODES) / sizeof(ADDNODES[0]);
---
+++ src/revs.hpp
@@ -0,0 +1,21 @@
+#ifndef REVS_HPP
+#define REVS_HPP
+
+#include <string>
+
+extern const std::string NAME;
+extern const std::string SUPPLY;
+extern const std::string REWARD;
+extern const std::string END;
+extern const std::string ADAPTIVEPOW;
+extern const std::string ERAS;
+extern const std::string BLOCKTIME;
+extern const std::string Cbmaturity;
+extern const std::string STAKED;
+extern const std::string SAPLING;
+extern const std::string PERC;
+extern const std::string PUBKEY;
+extern const std::string ADDNODES[];
+extern const int ADDNODES_COUNT;
+
+#endif // REVS_HPP
